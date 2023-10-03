522be5eaf233873617b4bbf8ca899e44e7898937
Koushik-Dey <77333183+Koushik-Dey@users.noreply.github.com>
2023-10-03 08:29:39 +0000

    datatypes

---
+++ js/02_datatypes.js
@@ -0,0 +1,30 @@
+'use strict'; // treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs, not brwoser
+
+//code readibility shoud be high
+//like
+
+console.log(3 + 3)
+
+console.log("koushik")
+
+let name = "koushik"
+let age = 18
+let isloggedIn = false
+
+// primitive data types
+
+// number => 2 to power 53
+// bigInt 
+//string => ""
+// boolean => true or false
+// null => standablone value
+// undefined
+//symbol => unique
+
+// non primitive datatypes
+
+// object
+
+console.log(typeof null)
