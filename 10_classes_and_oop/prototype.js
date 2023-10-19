1cb9cce8a4c7d6cc5d30b201c317a4f47b018e29
Koushik-Dey <77333183+Koushik-Dey@users.noreply.github.com>
2023-10-19 10:54:20 +0000

    prototype in depth

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,28 @@
+let myName = "koushik   "
+let myChannel = "coffe  "
+
+
+
+// console.log(myName.trueLength)
+
+
+let myHeroes = ['thor',"spiderman"]
+
+let heroPower = {
+    thor:"hammer",
+    spiderman:"sling",
+
+    getSpiderPower:function () {
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
+Object.prototype.koushik = function () {
+    console.log(`koushik is present in all object`)
+}
+
+Arr
+
+
+// heroPower.koushik()
+myHeroes.koushik()
