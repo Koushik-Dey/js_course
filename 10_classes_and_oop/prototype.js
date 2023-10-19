e5b9ca51bdc79c21cf9e0aee90ba14ed53eb85b6
Koushik-Dey <77333183+Koushik-Dey@users.noreply.github.com>
2023-10-19 11:14:06 +0000

    prototype in depth

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -21,8 +21,55 @@ Object.prototype.koushik = function () {
     console.log(`koushik is present in all object`)
 }
 
-Arr
+Array.prototype.heyKoushik = function () {
+    console.log(`Koushik say hello to all`)
+}
 
 
 // heroPower.koushik()
 myHeroes.koushik()
+
+myHeroes.heyKoushik()
+
+// heroPower.heyKoushik()
+
+
+//inheritance
+
+const User = {
+    name:"koushik",
+    isLoggedIn:true,
+    email:"kd@gmail.com"
+}
+
+const Teacher = {
+    makeVideo:true
+}
+
+const TeachingSupport = {
+    isAvailable:false
+}
+
+const TASupport = {
+    makeAssignMent:"JS assignment",
+    fullTime:true,
+    __proto__:TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+//modern syntax
+
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+let anotherUserName = "ChaiAurCode     "
+
+String.prototype.trueLength = function () {
+    console.log(`${this}`)
+    console.log(`True length is: ${this.trim().length}`)
+}
+
+anotherUserName.trueLength()
+
+"koushik".trueLength()
+"tea".trueLength()
