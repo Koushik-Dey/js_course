877fa3bce817491112c097b48ce7e937d824c0a2
Koushik-Dey <kd89601@gmail.com>
2023-12-15 19:19:41 +0530

    dom-projects

--- 07_projects/projectTwo/script.js
+++ 07_projects/projectTwo/script.js
@@ -3,17 +3,22 @@ let form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let height = document.getElementById("height").value;
-  let weight = document.getElementById("weight").value;
+  let height = parseInt(document.getElementById("height").value);
+  let weight = parseInt(document.getElementById("weight").value);
   let results = document.querySelector("#results");
 
-  if (height !== "" && weight !== "") {
-    let bmi = (weight / height / height) * 10000;
-    results.innerHTML = `Your BMI is ${bmi.toFixed(2)}`;
-
-    height = "";
-    weight = "";
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `<p>Please enter a valid height ${height}</p>`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `<p>Please enter a valid weight ${weight}</p>`;
   } else {
-    results.innerHTML = "Please enter valid values";
+    let bmi = (weight / height / height) * 10000;
+    if (bmi <= 18.6) {
+      results.innerHTML = `<p>under weight and bmi is${bmi.toFixed(2)}</p>`;
+    } else if (bmi > 18.6 && bmi <= 24.9) {
+      results.innerHTML = `<p>Normal Range and bmi is${bmi.toFixed(2)}</p>`;
+    } else {
+      results.innerHTML = `<p>Overweight and bmi is ${bmi.toFixed(2)}</p>`;
+    }
   }
 });
