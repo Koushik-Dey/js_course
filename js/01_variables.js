aff03c3325f052fab9a6c045fa9276148d187017
Koushik-Dey <kd89601@gmail.com>
2023-12-18 15:24:19 +0530

    xmlHTTPrequest

--- js/01_variables.js
+++ js/01_variables.js
@@ -1,16 +1,16 @@
 const accountId = 144553;
-let accountEmail = "Koushik@gmai.com"
-var accountPassword = "12345"
-accountCity = "Kolkata"
+let accountEmail = "Koushik@gmai.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
 let accountState;
 
 // accountId = 2;
 
-accountEmail = "kd89601@gmail.com"
-accountPassword = "rollsroy4u"
-accountCity = "Bangaluru"
+accountEmail = "kd89601@gmail.com";
+accountPassword = "rollsroy4u";
+accountCity = "Bangaluru";
 
-console.log(accountId)
+console.log(accountId);
 
 /*
     perfer not to use var
@@ -18,4 +18,18 @@ console.log(accountId)
 
 */
 
-console.table([accountId , accountEmail,accountPassword,accountCity , accountState]);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+function quiz() {
+  const x = 10;
+  delete x;
+  return x;
+}
+
+console.log(quiz());
